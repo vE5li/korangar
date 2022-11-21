c394d8879e9a1d1a747c912e42e74a0f4e45e4f8
ve5li <ve5li@tuta.io>
2022-11-22 00:22:26 +0100

    enable colors by default on unix machines

--- src/debug/logging/colors.rs
+++ src/debug/logging/colors.rs
@@ -1,13 +1,21 @@
-const fn color(enabled: &'static str) -> &'static str {
-    match cfg!(feature = "colors") {
-        true => enabled,
-        false => "",
-    }
+#[cfg(any(target_os = "windows", feature = "plain"))]
+mod colors {
+    pub const GREEN: &str = "";
+    pub const RED: &str = "";
+    pub const CYAN: &str = "";
+    pub const YELLOW: &str = "";
+    pub const MAGENTA: &str = "";
+    pub const NONE: &str = "";
 }
 
-pub const GREEN: &str = color("\x1B[32m");
-pub const RED: &str = color("\x1B[31m");
-pub const CYAN: &str = color("\x1B[36m");
-pub const YELLOW: &str = color("\x1B[33m");
-pub const MAGENTA: &str = color("\x1B[35m");
-pub const NONE: &str = color("\x1B[0m");
+#[cfg(not(any(target_os = "windows", feature = "plain")))]
+mod colors {
+    pub const GREEN: &str = "\x1B[32m";
+    pub const RED: &str = "\x1B[31m";
+    pub const CYAN: &str = "\x1B[36m";
+    pub const YELLOW: &str = "\x1B[33m";
+    pub const MAGENTA: &str = "\x1B[35m";
+    pub const NONE: &str = "\x1B[0m";
+}
+
+pub use self::colors::*;

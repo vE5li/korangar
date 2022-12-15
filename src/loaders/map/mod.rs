39d89e54e6803cc0e6fc64d0ebf5ea5e10554826
Julio Labarca <jlabarca25@gmail.com>
2022-12-15 00:16:10 -0300

    Fixed offset issues and some code cleanup

--- src/loaders/map/mod.rs
+++ src/loaders/map/mod.rs
@@ -2,6 +2,7 @@ pub mod map_data;
 pub mod resource;
 
 use std::collections::HashMap;
+use std::slice::IterMut;
 use std::sync::Arc;
 
 use cgmath::{Deg, Vector2, Vector3};
@@ -93,113 +94,82 @@ impl MapLoader {
         water_settings.water_level = Some(-water_settings.water_level.unwrap());
         let water_level = water_settings.water_level.unwrap();
 
-        let ground_data = parse_ground_data(map_data.ground_file, game_file_loader)?;
+        let ground_data = parse_ground_data(map_data.ground_file.as_str(), game_file_loader, texture_loader)?;
 
-        let mut gat_data = parse_gat_data(map_data.gat_file.unwrap(), game_file_loader)?;
+        let mut gat_data = parse_gat_data(map_data.gat_file.unwrap().as_str(), game_file_loader)?;
 
-        //Load Textures
-        let mut textures = Vec::new();
-        ground_data.textures.iter().for_each(|texture_name| {
-            let texture = texture_loader.get(&texture_name, game_file_loader).unwrap();
-            textures.push(texture);
-        });
+        let (tile_vertices, tile_picker_vertices) = generate_vertices(&mut gat_data);
+        let (tile_vertex_buffer, tile_picker_vertex_buffer, native_ground_vertices, water_vertices) = self.generate_vertices(tile_vertices, tile_picker_vertices, &ground_data, water_level);
+        let ground_vertices = NativeModelVertex::to_vertices(native_ground_vertices);
+        let ground_vertex_buffer = self.generate_ground_vertex_buffer(ground_vertices);
+        let water_vertex_buffer = self.generate_water_vertex_buffer(water_vertices);
 
-        let mut tile_vertices = Vec::new();
-        let mut tile_picker_vertices = Vec::new();
+        apply_map_offset(&ground_data, &mut map_data.resources);
 
-        //gat_data.tiles.iter_mut().for_each(|tile| {
-        let mut count = 0;
-        for y in 0..gat_data.map_height {
-            for x in 0..gat_data.map_width {
-                let mut tile = &mut gat_data.tiles[count];
-                //replace with attribute
-                tile.upper_left_height = -tile.upper_left_height;
-                tile.upper_right_height = -tile.upper_right_height;
-                tile.lower_left_height = -tile.lower_left_height;
-                tile.lower_right_height = -tile.lower_right_height;
-                count += 1;
+        let textures = load_textures(&ground_data, texture_loader, game_file_loader);
 
-                if tile.tile_type.is_none() {
-                    continue;
-                }
 
-                let offset = Vector2::new(x as f32 * 5.0, y as f32 * 5.0);
+        let map = Arc::new(Map::new(
+            map_data.version,
+            ground_data.version,
+            gat_data.map_width as usize,
+            gat_data.map_height as usize,
+            map_data.water_settings,
+            map_data.light_settings,
+            gat_data.tiles,
+            ground_vertex_buffer,
+            water_vertex_buffer,
+            textures,
+            map_data.resources.objects,
+            map_data.resources.light_sources,
+            map_data.resources.sound_sources,
+            map_data.resources.effect_sources,
+            tile_picker_vertex_buffer.unwrap(),
+            tile_vertex_buffer.unwrap(),
+        ));
 
-                let first_position = Vector3::new(offset.x, tile.upper_left_height + 1.0, offset.y);
-                let second_position = Vector3::new(offset.x + 5.0, tile.upper_right_height + 1.0, offset.y);
-                let third_position = Vector3::new(offset.x + 5.0, tile.lower_right_height + 1.0, offset.y + 5.0);
-                let fourth_position = Vector3::new(offset.x, tile.lower_left_height + 1.0, offset.y + 5.0);
+        self.cache.insert(resource_file, map.clone());
 
-                let first_normal = NativeModelVertex::calculate_normal(first_position, second_position, third_position);
-                let second_normal = NativeModelVertex::calculate_normal(fourth_position, first_position, third_position);
+        #[cfg(feature = "debug")]
+        timer.stop();
 
-                let first_texture_coordinates = Vector2::new(0.0, 0.0);
-                let second_texture_coordinates = Vector2::new(0.0, 1.0);
-                let third_texture_coordinates = Vector2::new(1.0, 1.0);
-                let fourth_texture_coordinates = Vector2::new(1.0, 0.0);
+        Ok(map)
+    }
 
-                let tile_type_index = tile.tile_type.0 as i32;
+    fn generate_water_vertex_buffer(&mut self, water_vertices: Vec<WaterVertex>) -> Option<Arc<CpuAccessibleBuffer<[WaterVertex]>>> {
+        let water_vertex_buffer = match !water_vertices.is_empty() {
+            true => CpuAccessibleBuffer::from_iter(
+                &*self.memory_allocator,
+                BufferUsage {
+                    vertex_buffer: true,
+                    ..Default::default()
+                },
+                false,
+                water_vertices.into_iter(),
+            )
+            .unwrap()
+            .into(),
+            false => None,
+        };
+        water_vertex_buffer
+    }
 
-                tile_vertices.push(ModelVertex::new(
-                    first_position,
-                    first_normal,
-                    first_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
-                tile_vertices.push(ModelVertex::new(
-                    second_position,
-                    first_normal,
-                    second_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
-                tile_vertices.push(ModelVertex::new(
-                    third_position,
-                    first_normal,
-                    third_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
+    fn generate_ground_vertex_buffer(&mut self, ground_vertices: Vec<ModelVertex>) -> Arc<CpuAccessibleBuffer<[ModelVertex]>> {
+        let ground_vertex_buffer = CpuAccessibleBuffer::from_iter(
+            &*self.memory_allocator,
+            BufferUsage {
+                vertex_buffer: true,
+                ..Default::default()
+            },
+            false,
+            ground_vertices.into_iter(),
+        )
+        .unwrap();
+        ground_vertex_buffer
+    }
 
-                tile_vertices.push(ModelVertex::new(
-                    first_position,
-                    second_normal,
-                    first_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
-                tile_vertices.push(ModelVertex::new(
-                    third_position,
-                    second_normal,
-                    third_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
-                tile_vertices.push(ModelVertex::new(
-                    fourth_position,
-                    second_normal,
-                    fourth_texture_coordinates,
-                    tile_type_index,
-                    0.0,
-                ));
-
-                let first_position = Vector3::new(offset.x, tile.upper_left_height, offset.y);
-                let second_position = Vector3::new(offset.x + 5.0, tile.upper_right_height, offset.y);
-                let third_position = Vector3::new(offset.x + 5.0, tile.lower_right_height, offset.y + 5.0);
-                let fourth_position = Vector3::new(offset.x, tile.lower_left_height, offset.y + 5.0);
-
-                let color = PickerTarget::Tile(x as u16, y as u16).into();
-                tile_picker_vertices.push(TileVertex::new(first_position, color));
-                tile_picker_vertices.push(TileVertex::new(second_position, color));
-                tile_picker_vertices.push(TileVertex::new(third_position, color));
-
-                tile_picker_vertices.push(TileVertex::new(first_position, color));
-                tile_picker_vertices.push(TileVertex::new(third_position, color));
-                tile_picker_vertices.push(TileVertex::new(fourth_position, color));
-            }
-        }
-
+    fn generate_vertices(&mut self, tile_vertices: Vec<ModelVertex>, tile_picker_vertices: Vec<TileVertex>, ground_data: &GroundData, water_level: f32)
+     -> (Option<Arc<CpuAccessibleBuffer<[ModelVertex]>>>, Option<Arc<CpuAccessibleBuffer<[TileVertex]>>>, Vec<NativeModelVertex>, Vec<WaterVertex>) {
         let vertex_buffer = CpuAccessibleBuffer::from_iter(
             &*self.memory_allocator,
             BufferUsage {
@@ -210,9 +180,7 @@ impl MapLoader {
             tile_vertices.into_iter(),
         )
         .unwrap();
-
         let tile_vertex_buffer = Some(vertex_buffer);
-
         let vertex_buffer = CpuAccessibleBuffer::from_iter(
             &*self.memory_allocator,
             BufferUsage {
@@ -223,12 +191,10 @@ impl MapLoader {
             tile_picker_vertices.into_iter(),
         )
         .unwrap();
-
         //vertices calculation
         let tile_picker_vertex_buffer = Some(vertex_buffer);
         let mut native_ground_vertices = Vec::new();
         let mut water_vertices = Vec::new();
-
         //ground_data.ground_tiles.iter().for_each(|ground_tile| {
         let width = ground_data.width as usize;
         let height = ground_data.height as usize;
@@ -354,81 +320,7 @@ impl MapLoader {
                 }
             }
         }
-
-        let ground_vertices = NativeModelVertex::to_vertices(native_ground_vertices);
-        let ground_vertex_buffer = CpuAccessibleBuffer::from_iter(
-            &*self.memory_allocator,
-            BufferUsage {
-                vertex_buffer: true,
-                ..Default::default()
-            },
-            false,
-            ground_vertices.into_iter(),
-        )
-        .unwrap();
-
-        let water_vertex_buffer = match !water_vertices.is_empty() {
-            true => CpuAccessibleBuffer::from_iter(
-                &*self.memory_allocator,
-                BufferUsage {
-                    vertex_buffer: true,
-                    ..Default::default()
-                },
-                false,
-                water_vertices.into_iter(),
-            )
-            .unwrap()
-            .into(),
-            false => None,
-        };
-
-        let offset = Vector3::new(
-            gat_data.map_width as f32 * MAP_OFFSET,
-            0.0,
-            gat_data.map_height as f32 * MAP_OFFSET,
-        );
-        map_data.resources.objects.iter_mut().for_each(|object| object.offset(offset));
-        map_data
-            .resources
-            .objects
-            .iter_mut()
-            .for_each(|sound_source| sound_source.offset(offset));
-        map_data
-            .resources
-            .objects
-            .iter_mut()
-            .for_each(|light_source| light_source.offset(offset));
-        map_data
-            .resources
-            .objects
-            .iter_mut()
-            .for_each(|effect_source| effect_source.offset(offset));
-
-        let map = Arc::new(Map::new(
-            map_data.version,
-            ground_data.version,
-            gat_data.map_width as usize,
-            gat_data.map_height as usize,
-            map_data.water_settings,
-            map_data.light_settings,
-            gat_data.tiles,
-            ground_vertex_buffer,
-            water_vertex_buffer,
-            textures,
-            map_data.resources.objects,
-            map_data.resources.light_source,
-            map_data.resources.sound_source,
-            map_data.resources.effect_source,
-            tile_picker_vertex_buffer.unwrap(),
-            tile_vertex_buffer.unwrap(),
-        ));
-
-        self.cache.insert(resource_file, map.clone());
-
-        #[cfg(feature = "debug")]
-        timer.stop();
-
-        Ok(map)
+        (tile_vertex_buffer, tile_picker_vertex_buffer, native_ground_vertices, water_vertices)
     }
 
     pub fn get(
@@ -443,6 +335,134 @@ impl MapLoader {
             None => self.load(resource_file, game_file_loader, model_loader, texture_loader),
         }
     }
+}
+
+fn load_textures(ground_data: &GroundData, texture_loader: &mut TextureLoader, game_file_loader: &mut GameFileLoader) -> Vec<Texture> {
+    let mut textures = Vec::new();
+    ground_data.textures.iter().for_each(|texture_name| {
+        let texture = texture_loader.get(&texture_name, game_file_loader).unwrap();
+        textures.push(texture);
+    });
+    textures
+}
+
+fn generate_vertices(gat_data: &mut GatData) -> (Vec<ModelVertex>, Vec<TileVertex>) {
+    let mut tile_vertices = Vec::new();
+    let mut tile_picker_vertices = Vec::new();
+    //gat_data.tiles.iter_mut().for_each(|tile| {
+    let mut count = 0;
+    for y in 0..gat_data.map_height {
+        for x in 0..gat_data.map_width {
+            let mut tile = &mut gat_data.tiles[count];
+            //replace with attribute
+            tile.upper_left_height = -tile.upper_left_height;
+            tile.upper_right_height = -tile.upper_right_height;
+            tile.lower_left_height = -tile.lower_left_height;
+            tile.lower_right_height = -tile.lower_right_height;
+            count += 1;
+
+            if tile.tile_type.is_none() {
+                continue;
+            }
+
+            let offset = Vector2::new(x as f32 * 5.0, y as f32 * 5.0);
+
+            let first_position = Vector3::new(offset.x, tile.upper_left_height + 1.0, offset.y);
+            let second_position = Vector3::new(offset.x + 5.0, tile.upper_right_height + 1.0, offset.y);
+            let third_position = Vector3::new(offset.x + 5.0, tile.lower_right_height + 1.0, offset.y + 5.0);
+            let fourth_position = Vector3::new(offset.x, tile.lower_left_height + 1.0, offset.y + 5.0);
+
+            let first_normal = NativeModelVertex::calculate_normal(first_position, second_position, third_position);
+            let second_normal = NativeModelVertex::calculate_normal(fourth_position, first_position, third_position);
+
+            let first_texture_coordinates = Vector2::new(0.0, 0.0);
+            let second_texture_coordinates = Vector2::new(0.0, 1.0);
+            let third_texture_coordinates = Vector2::new(1.0, 1.0);
+            let fourth_texture_coordinates = Vector2::new(1.0, 0.0);
+
+            let tile_type_index = tile.tile_type.0 as i32;
+
+            tile_vertices.push(ModelVertex::new(
+                first_position,
+                first_normal,
+                first_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+            tile_vertices.push(ModelVertex::new(
+                second_position,
+                first_normal,
+                second_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+            tile_vertices.push(ModelVertex::new(
+                third_position,
+                first_normal,
+                third_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+
+            tile_vertices.push(ModelVertex::new(
+                first_position,
+                second_normal,
+                first_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+            tile_vertices.push(ModelVertex::new(
+                third_position,
+                second_normal,
+                third_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+            tile_vertices.push(ModelVertex::new(
+                fourth_position,
+                second_normal,
+                fourth_texture_coordinates,
+                tile_type_index,
+                0.0,
+            ));
+
+            let first_position = Vector3::new(offset.x, tile.upper_left_height, offset.y);
+            let second_position = Vector3::new(offset.x + 5.0, tile.upper_right_height, offset.y);
+            let third_position = Vector3::new(offset.x + 5.0, tile.lower_right_height, offset.y + 5.0);
+            let fourth_position = Vector3::new(offset.x, tile.lower_left_height, offset.y + 5.0);
+
+            let color = PickerTarget::Tile(x as u16, y as u16).into();
+            tile_picker_vertices.push(TileVertex::new(first_position, color));
+            tile_picker_vertices.push(TileVertex::new(second_position, color));
+            tile_picker_vertices.push(TileVertex::new(third_position, color));
+
+            tile_picker_vertices.push(TileVertex::new(first_position, color));
+            tile_picker_vertices.push(TileVertex::new(third_position, color));
+            tile_picker_vertices.push(TileVertex::new(fourth_position, color));
+        }
+    }
+    (tile_vertices, tile_picker_vertices)
+}
+
+fn apply_map_offset(ground_data: &GroundData, resources: &mut MapResources) {
+    let offset = Vector3::new(
+        ground_data.width as f32 * MAP_OFFSET,
+        0.0,
+        ground_data.height as f32 * MAP_OFFSET,
+    );
+    resources.objects.iter_mut().for_each(|object| object.offset(offset));
+    resources
+        .sound_sources
+        .iter_mut()
+        .for_each(|sound_source| sound_source.offset(offset));
+    resources
+        .light_sources
+        .iter_mut()
+        .for_each(|light_source| light_source.offset(offset));
+    resources
+        .effect_sources
+        .iter_mut()
+        .for_each(|effect_source| effect_source.offset(offset));
 }
 
 fn parse_map_data(
@@ -473,7 +493,7 @@ fn parse_map_data(
     Ok(map_data)
 }
 
-fn parse_ground_data(ground_file: String, game_file_loader: &mut GameFileLoader) -> Result<GroundData, String> {
+fn parse_ground_data(ground_file: &str, game_file_loader: &mut GameFileLoader, texture_loader: &mut TextureLoader) -> Result<GroundData, String> {
     let bytes = game_file_loader.get(&format!("data\\{}", &ground_file))?;
     let mut byte_stream = ByteStream::new(&bytes);
     let magic = byte_stream.string(4);
@@ -487,7 +507,7 @@ fn parse_ground_data(ground_file: String, game_file_loader: &mut GameFileLoader)
     Ok(ground_data)
 }
 
-fn parse_gat_data(gat_file: String, game_file_loader: &mut GameFileLoader) -> Result<GatData, String> {
+fn parse_gat_data(gat_file: &str, game_file_loader: &mut GameFileLoader) -> Result<GatData, String> {
     let bytes = game_file_loader.get(&format!("data\\{}", &gat_file))?;
     let mut byte_stream = ByteStream::new(&bytes);
     let magic = byte_stream.string(4);

3e9456c01458153020fbb43826d65e9b5e00e653
vE5li <ve5li@tuta.io>
2024-02-13 03:40:11 +0100

    Remove unused cfg attributes

--- src/interface/windows/debug/mod.rs
+++ src/interface/windows/debug/mod.rs
@@ -1,16 +1,13 @@
 mod commands;
 mod inspector;
 mod maps;
-#[cfg(feature = "debug")]
 mod packet;
 mod profiler;
 mod time;
 
 pub use self::commands::CommandsWindow;
-#[cfg(feature = "debug")]
 pub use self::inspector::FrameInspectorWindow;
 pub use self::maps::MapsWindow;
-#[cfg(feature = "debug")]
 pub use self::packet::PacketWindow;
 pub use self::profiler::ProfilerWindow;
 pub use self::time::TimeWindow;

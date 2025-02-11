3894a1eb3d8e7aad10ce22e6b3f6ea9c2913c0a1
Nils Hasenbanck <nils@hasenbanck.de>
2025-02-11 16:45:29 +0100

    Pre-notify winit that we present the next frame
    This enables winit to reason about the internal state. For example, it can properly throttle WindowEvent::RedrawRequested under Wayland.

--- korangar/src/main.rs
+++ korangar/src/main.rs
@@ -1924,6 +1924,8 @@ impl Client {
         #[cfg(feature = "debug")]
         loads_measurement.stop();
 
+        let window = self.window.as_ref().expect("window missing");
+
         // Main map update and render loop
         if let Some(map) = self.map.as_ref() {
             #[cfg(feature = "debug")]
@@ -2417,7 +2419,7 @@ impl Client {
                 marker: self.debug_marker_renderer.get_instructions(),
             };
 
-            self.graphics_engine.render_next_frame(frame, render_instruction);
+            self.graphics_engine.render_next_frame(window, frame, render_instruction);
 
             #[cfg(feature = "debug")]
             render_frame_measurement.stop();
@@ -2425,7 +2427,7 @@ impl Client {
             #[cfg(feature = "debug")]
             let render_frame_measurement = Profiler::start_measurement("render next frame");
 
-            self.graphics_engine.render_next_frame(frame, RenderInstruction::default());
+            self.graphics_engine.render_next_frame(window, frame, RenderInstruction::default());
 
             #[cfg(feature = "debug")]
             render_frame_measurement.stop();

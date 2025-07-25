cb550195e45b24f9b629999bfd880ea3bc900b48
Nils Hasenbanck <nils@hasenbanck.de>
2025-07-25 11:33:48 +0200

    Use better multithreading for 7z archives

--- korangar/src/loaders/archive/seven_zip/mod.rs
+++ korangar/src/loaders/archive/seven_zip/mod.rs
@@ -2,38 +2,49 @@
 
 mod builder;
 
+use std::cell::RefCell;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{Seek, SeekFrom};
 use std::path::{Path, PathBuf};
-use std::sync::Mutex;
 
 use blake3::Hasher;
 use hashbrown::HashMap;
 #[cfg(feature = "debug")]
 use korangar_debug::logging::{Colorize, Timer, print_debug};
-use sevenz_rust2::{ArchiveReader, EncoderMethod, Password};
+use sevenz_rust2::BlockDecoder;
 
 pub use self::builder::SevenZipArchiveBuilder;
 use crate::loaders::archive::{Archive, Compression};
 
+const MB_1: u64 = 1024 * 1024;
+const MB_4: u64 = 4 * MB_1;
+const MB_8: u64 = 8 * MB_1;
+
+thread_local! {
+    static FILE_CACHE: RefCell<HashMap<PathBuf, File>> = RefCell::new(HashMap::new());
+}
+
 struct FileIndexEntry {
     compression: Compression,
-    file_name: String,
+    file_crc: u64,
+    file_size: u64,
+    block_index: usize,
 }
 
 pub struct SevenZipArchive {
-    pub(crate) reader: Mutex<ArchiveReader<BufReader<File>>>,
-    file_index: HashMap<String, FileIndexEntry>,
+    archive: sevenz_rust2::Archive,
+    password: sevenz_rust2::Password,
+    file_lookup: HashMap<String, FileIndexEntry>,
     file_path: PathBuf,
 }
 
 impl SevenZipArchive {
     pub fn file_exists(&self, asset_path: &str) -> bool {
-        self.file_index.contains_key(asset_path)
+        self.file_lookup.contains_key(asset_path)
     }
 
     pub fn file_is_compressed(&self, asset_path: &str) -> Option<Compression> {
-        self.file_index.get(asset_path).map(|entry| entry.compression)
+        self.file_lookup.get(asset_path).map(|entry| entry.compression)
     }
 }
 
@@ -41,59 +52,104 @@ impl Archive for SevenZipArchive {
     fn from_path(path: &Path) -> Self {
         #[cfg(feature = "debug")]
         let timer = Timer::new_dynamic(format!("load game data from {}", path.display().magenta()));
-        let file = File::open(path).expect("can't open archive");
-        let reader = ArchiveReader::new(BufReader::new(file), Password::empty()).unwrap();
-        let archive = reader.archive();
+        let mut archive_file = File::open(path).expect("can't open archive");
+        let password = sevenz_rust2::Password::empty();
+
+        let archive = sevenz_rust2::Archive::read(&mut archive_file, &password).expect("can't read archive");
 
         assert!(!archive.is_solid, "7zip archives needs to be non-solid for fast file access");
 
-        let mut compression_methods = Vec::new();
-        let mut file_index = HashMap::with_capacity(archive.files.len());
-        archive.files.iter().for_each(|file| {
-            let name_with_backslash = file.name().replace('/', "\\").to_lowercase();
+        let mut file_lookup = HashMap::with_capacity(archive.files.len());
 
-            compression_methods.clear();
-            reader
-                .file_compression_methods(file.name(), &mut compression_methods)
-                .expect("can't read compression methods of file");
-
-            let compression = if compression_methods.iter().any(|method| method.id() == EncoderMethod::ID_COPY) {
-                Compression::Off
-            } else {
-                Compression::Default
+        for (file_index, file) in archive.files.iter().enumerate() {
+            let Some(block_index) = archive.stream_map.file_block_index[file_index] else {
+                continue;
             };
 
-            file_index.insert(name_with_backslash, FileIndexEntry {
+            let name_with_backslash = file.name().replace('/', "\\").to_lowercase();
+
+            let block = &archive.blocks[block_index];
+
+            let compression = match block
+                .coders
+                .iter()
+                .any(|coder| coder.encoder_method_id() == sevenz_rust2::EncoderMethod::ID_COPY)
+            {
+                true => Compression::Off,
+                false => Compression::Default,
+            };
+
+            file_lookup.insert(name_with_backslash, FileIndexEntry {
                 compression,
-                file_name: file.name().to_string(),
+                file_crc: file.crc,
+                file_size: file.size,
+                block_index,
             });
-        });
+        }
 
         #[cfg(feature = "debug")]
         timer.stop();
 
         Self {
-            reader: Mutex::new(reader),
-            file_index,
+            archive,
+            password,
+            file_lookup,
             file_path: PathBuf::from(path),
         }
     }
 
     fn file_exists(&self, asset_path: &str) -> bool {
-        self.file_index.contains_key(asset_path)
+        self.file_lookup.contains_key(asset_path)
     }
 
     fn get_file_by_path(&self, asset_path: &str) -> Option<Vec<u8>> {
-        self.file_index
-            .get(asset_path)
-            .and_then(|entry| self.reader.lock().unwrap().read_file(entry.file_name.as_str()).ok())
+        let file_entry = self.file_lookup.get(asset_path)?;
+
+        FILE_CACHE.with(|cache| {
+            let mut cache = cache.borrow_mut();
+
+            let archive_file = cache
+                .entry(self.file_path.clone())
+                .or_insert_with(|| File::open(&self.file_path).expect("can't open archive file"));
+            archive_file.seek(SeekFrom::Start(0)).expect("can't seek to file start");
+
+            let thread_count = match file_entry.compression {
+                Compression::Default if file_entry.file_size > MB_8 => 8,
+                Compression::Default if file_entry.file_size > MB_4 => 4,
+                Compression::Default if file_entry.file_size > MB_1 => 2,
+                _ => 1,
+            };
+
+            let block_decoder = BlockDecoder::new(
+                thread_count,
+                file_entry.block_index,
+                &self.archive,
+                &self.password,
+                archive_file,
+            );
+
+            let mut data = vec![0; file_entry.file_size as usize];
+            let mut found = false;
+
+            block_decoder
+                .for_each_entries(&mut |entry, reader| {
+                    if entry.crc == file_entry.file_crc && entry.size == file_entry.file_size {
+                        let _ = reader.read_exact(&mut data);
+                        found = true;
+                    }
+                    Ok(false)
+                })
+                .expect("could not read entry");
+
+            match found {
+                true => Some(data),
+                false => None,
+            }
+        })
     }
 
     fn get_files_with_extension(&self, files: &mut Vec<String>, extensions: &[&str]) {
-        self.reader
-            .lock()
-            .unwrap()
-            .archive()
+        self.archive
             .files
             .iter()
             .filter(|file| {

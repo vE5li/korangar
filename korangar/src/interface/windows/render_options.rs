9917925a2c50f70983786ffe522efee956cdaf66
vE5li <ve5li@tuta.io>
2025-07-27 01:35:52 +0200

    Capitalize render options

--- korangar/src/interface/windows/render_options.rs
+++ korangar/src/interface/windows/render_options.rs
@@ -80,66 +80,66 @@ where
 
         let elements = (
             collapsable! {
-                text: "general",
+                text: "General",
                 initially_expanded: true,
                 children: (
-                    render_state_button("debug camera", self.path.use_debug_camera()),
-                    render_state_button("show fps", self.path.show_frames_per_second()),
-                    render_state_button("show wireframe", self.path.show_wireframe()),
-                    render_state_button("frustum culling", self.path.frustum_culling()),
-                    render_state_button("show bounding boxes", self.path.show_bounding_boxes()),
-                    render_state_button("show entities debug", self.path.show_entities_debug()),
-                    render_state_button("show entities paper", self.path.show_entities_paper()),
+                    render_state_button("Debug camera", self.path.use_debug_camera()),
+                    render_state_button("Show fps", self.path.show_frames_per_second()),
+                    render_state_button("Show wireframe", self.path.show_wireframe()),
+                    render_state_button("Frustum culling", self.path.frustum_culling()),
+                    render_state_button("Show bounding boxes", self.path.show_bounding_boxes()),
+                    render_state_button("Show entities debug", self.path.show_entities_debug()),
+                    render_state_button("Show entities paper", self.path.show_entities_paper()),
                 ),
             },
             collapsable! {
-                text: "map",
+                text: "Map",
                 initially_expanded: true,
                 children: (
-                    render_state_button("show map", self.path.show_map()),
-                    render_state_button("show objects", self.path.show_objects()),
-                    render_state_button("show entities", self.path.show_entities()),
-                    render_state_button("show water", self.path.show_water()),
-                    render_state_button("show indicators", self.path.show_indicators()),
+                    render_state_button("Show map", self.path.show_map()),
+                    render_state_button("Show objects", self.path.show_objects()),
+                    render_state_button("Show entities", self.path.show_entities()),
+                    render_state_button("Show water", self.path.show_water()),
+                    render_state_button("Show indicators", self.path.show_indicators()),
                 )
             },
             collapsable! {
-                text: "lighting",
+                text: "Lighting",
                 initially_expanded: true,
                 children: (
-                    render_state_button("ambient light", self.path.show_ambient_light()),
-                    render_state_button( "directional light", self.path.show_directional_light()),
-                    render_state_button("point lights", self.path.show_point_lights()),
-                    render_state_button("particle lights", self.path.show_particle_lights()),
+                    render_state_button("Ambient light", self.path.show_ambient_light()),
+                    render_state_button("Directional light", self.path.show_directional_light()),
+                    render_state_button("Point lights", self.path.show_point_lights()),
+                    render_state_button("Particle lights", self.path.show_particle_lights()),
                 )
             },
             collapsable! {
-                text: "markers",
+                text: "Markers",
                 initially_expanded: true,
                 children: (
-                    render_state_button("object markers", self.path.show_object_markers()),
-                    render_state_button("light markers", self.path.show_light_markers()),
-                    render_state_button("sound markers", self.path.show_sound_markers()),
-                    render_state_button("effect markers", self.path.show_effect_markers()),
-                    render_state_button("particle markers", self.path.show_particle_markers()),
-                    render_state_button("entity markers", self.path.show_entity_markers()),
-                    render_state_button("shadow markers", self.path.show_shadow_markers()),
+                    render_state_button("Object markers", self.path.show_object_markers()),
+                    render_state_button("Light markers", self.path.show_light_markers()),
+                    render_state_button("Sound markers", self.path.show_sound_markers()),
+                    render_state_button("Effect markers", self.path.show_effect_markers()),
+                    render_state_button("Particle markers", self.path.show_particle_markers()),
+                    render_state_button("Entity markers", self.path.show_entity_markers()),
+                    render_state_button("Shadow markers", self.path.show_shadow_markers()),
                 )
             },
             collapsable! {
-                text: "grid",
+                text: "Grid",
                 initially_expanded: true,
                 children: (
-                    render_state_button("map tiles", self.path.show_map_tiles()),
-                    render_state_button("pathing", self.path.show_pathing()),
+                    render_state_button("Map tiles", self.path.show_map_tiles()),
+                    render_state_button("Pathing", self.path.show_pathing()),
                 )
             },
             collapsable! {
-                text: "buffers",
+                text: "Buffers",
                 initially_expanded: true,
                 children: (
-                    render_state_button("picker", self.path.show_picker_buffer()),
-                    render_state_button("directional shadow", self.path.show_directional_shadow_map()),
+                    render_state_button("Picker", self.path.show_picker_buffer()),
+                    render_state_button("Directional shadow", self.path.show_directional_shadow_map()),
                     // split! {
                     //     children: (
                     //         drop_down! {
@@ -149,8 +149,8 @@ where
                     //         }
                     //     ),
                     // },
-                    render_state_button("light cull count", self.path.show_light_culling_count_buffer()),
-                    render_state_button("font map", self.path.show_font_map()),
+                    render_state_button("Light cull count", self.path.show_light_culling_count_buffer()),
+                    render_state_button("Font map", self.path.show_font_map()),
                 )
             },
         );
